ee4ee9ab3ab8a67ebddd8dbdb355982ecd1a192f
Sachin Pruthi <spruthi50@gmail.com>
2021-07-26 12:00:28 +0530

    changes done

--- js/index.js
+++ js/index.js
@@ -1,19 +1,4 @@
-function openTab(evt, day) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(day).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  document.getElementById("defaultOpen").click();
-
-  var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion");
       var i;
 
       for (i = 0; i < acc.length; i++) {
@@ -27,6 +12,8 @@ function openTab(evt, day) {
           }
         });
       }
+
+  
 
       function myEnterFunction() {
         document.getElementsByClassName("name").display ="none";
